3a92b45359064dcd4ed72faa77758f8cfc7b69dd
Pedro Canuto <data.canuto@gmail.com>
2026-06-26 17:15:00 -0300

    Implement city card registration and display
    This update adds functionality to register and display two city cards with various attributes such as state, city code, population, area, GDP, and tourist points.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,105 @@
 #include <stdio.h>
+#include <string.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+int main(){
+    char estado1, estado2;
+    char codigo1[1], codigo2[1];
+    char nome1[50], nome2[50];
+    int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosturisticos1, pontosturisticos2;   
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    //Entrada de dados para cadastrar carta 1
+    printf("Carta 1");
+    printf("Informe nome do estado 1: \n");
+    scanf("%c", &estado1);
+    getchar(); 
+    printf("Informe o código da cidade: \n");
+    scanf("%c", &codigo1);
+    getchar();       
+    printf("Informe o nome da cidade: \n");
+    scanf("%s", nome1);
+    getchar();
+    printf("Informe a população: \n");
+    scanf("%d", &populacao1);
+    getchar();
+    printf("Informe a área da cidade: \n");
+    scanf("%f", &area1);
+    getchar();
+    printf("Informe o pib da cidade: \n");
+    scanf("%f", &pib1);
+    getchar();
+    printf("Informe o número de pontos turísticos: \n");
+    scanf("%d",&pontosturisticos1);
+    getchar();
+    //Mensagem de confirmação para carta 1
+    printf("Carta cadastrada com sucesso!\n");
+    printf("Carta 1)";
+    printf("Estado: %c\n",estado1);
+    printf("Código: %c01\n", estado1);
+    printf("Nome da cidade: %s\n",nome1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %f Km²\n",area1);
+    printf("PIB: %f bilhões de reais\n ",pib1);
+    printf("Número de pontos turísticos: %d\n",pontosturisticos1);
 
-  // Área para entrada de dados
+    //Entrada de dados para cadastrar carta 2
+    printf("Informe nome do estado 2: \n");
+    scanf("%c", &estado2);
+    getchar();
+    printf("Informe o código da cidade: \n");
+    scanf("%c", &codigo1);
+    getchar();   
+    printf("Informe o nome da cidade: \n");
+    scanf("%s", nome2);
+    getchar();
+    printf("Informe a população: \n");
+    scanf("%d", &populacao2);
+    getchar();
+    printf("Informe a área da cidade: \n");
+    scanf("%f", &area2);
+    getchar();
+    printf("Informe o pib da cidade: \n");
+    scanf("%f", &pib2);
+    getchar();
+    printf("Informe o número de pontos turísticos: \n");
+    scanf("%d",&pontosturisticos2);
+    getchar();
+     //Mensagem de confirmação para carta 2
+    printf("Carta cadastrada com sucesso!\n");
+    printf("Carta 2)";
+    printf("Estado: %c\n",estado2);
+    printf("Código: %c01\n", codigo2);
+    printf("Nome da cidade: %s\n",nome2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %f Km²\n",area2);
+    printf("PIB: %f bilhões de reais\n ",pib2);
+    printf("Número de pontos turísticos: %d\n",pontosturisticos2);
 
-  // Área para exibição dos dados da cidade
+    //Dados cadastrados
 
-return 0;
-} 
+    printf("Cartas cadastradas: \n");
+
+    printf("Carta 1\n");
+    printf("Estado: %c\n",estado1);
+    printf("Código: %c01\n", estado1);
+    printf("Nome da cidade: %s\n",nome1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %f Km²\n",area1);
+    printf("PIB: %f bilhões de reais\n ",pib1);
+    printf("Número de pontos turísticos: %d\n",pontosturisticos1);
+    printf("Carta 2\n");
+    printf("Estado: %c\n",estado2);
+    printf("Código: %c02\n", estado2);
+    printf("Nome da cidade: %s\n",nome2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %f Km²\n",area2);
+    printf("PIB: %f bilhões de reais\n",pib2);
+    printf("Número de pontos turísticos: %d\n",pontosturisticos2);
+
+    printf("\nPressione ENTER para encerrar o programa...\n");
+    getchar();
+    
+    return 0;
+}
